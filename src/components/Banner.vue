0ed86772726c1949ad7618b3ae5aad63d3b83989
Aurelienhs <aurelien.hebersuffrin@student.hogent.be>
2023-08-04 10:40:14 +0200

    Initialization Vue app with banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <img class="logo" src="@/assets/relivologo.png" alt="logo" />
+    Camera integration demo
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  components: {},
+};
+</script>
+
+<style>
+.header {
+  background: linear-gradient(#fe623d, #ff3472);
+  height: 50px;
+  font-size: 2em;
+  color: white;
+  padding: 20px;
+  text-align: center;
+}
+.logo {
+  width: 50p;
+  height: 50px;
+  float: left;
+}
+</style>
